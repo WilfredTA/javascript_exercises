2d8b272f2f470cb2f834597b8db27de254bba77e
Tannr Allard <rockinghamtannr@gmail.com>
2017-10-05 19:23:33 -0400

    custom array concatenation implementation

---
+++ custom_array_concat.js
@@ -0,0 +1,44 @@
+/*
+
+The first argument is an array. For simplicity, you can assume that this will always be the case.
+The concatenation returns a new array
+Follow the order of passing of the array arguments
+Copy object references from the source arrays to the new array; 
+	if you make a change to a reference object in one of the source arrays after calling concat, those changes should show up in the output array as well.
+For strings, numbers, and booleans the concatenation copies the values
+
+
+	Approach:
+
+	declare concatArray to a copy of first arg (which is an array)
+	Check if second argument is an array.
+		if it is, iterate through it, pushing its values to the concatArray
+		else, just push value to concatArray
+	return concatArray
+*/
+
+
+function concat(array, someValue) {
+	var concatArray = array.slice();
+
+	if (Array.isArray(someValue)) {
+		for (val in someValue) {
+			concatArray.push(someValue[val]);
+		}
+	} else {
+		concatArray.push(someValue)
+	}
+
+	console.log(concatArray);
+}
+
+var obj = { a: 2, b: 3 };
+var newArray = concat([2, 3], obj);      // [2, 3, { a: 2, b: 3 }]
+obj.a = 'two';
+console.log(newArray);                  // [2, 3, { a: 'two', b: 3 }]
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, obj];				 // [1, 2, 3, 4, 5, { a: 'two', b: 3 }]
+var arr3 = concat(arr1, arr2);
+
+
