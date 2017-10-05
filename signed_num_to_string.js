d45808e021a52121ad4d85e77adfe819607f22dd
Tannr Allard <rockinghamtannr@gmail.com>
2017-10-05 16:27:07 -0400

    Adding signed number to string solution

---
+++ signed_num_to_string.js
@@ -0,0 +1,47 @@
+
+/* 
+  Determine sign of number input
+  Remove sign
+  Get string of num
+  Re-apply sign
+
+*/
+
+function numberToString(num) {
+  var charNums = ['0', '1', '2', '3', '4', '5', '6' , '7', '8', '9'];
+
+  var stringNum = '';
+  var remainder;
+
+  do {
+    remainder = num % 10;
+    num = Math.floor(num / 10);
+
+    stringNum = charNums[remainder] + stringNum;
+  } while (num > 0);
+
+  return stringNum;
+}
+
+function signedNumToString(num) {
+  var sign;
+  var stringNum;
+
+  if (num < 0) {
+    sign = '-';
+    num *= -1;
+  } else {
+    sign = '+';
+  }
+
+  stringNum = numberToString(num);
+
+  stringNum = sign + stringNum;
+
+  return stringNum;
+
+}
+
+console.log(signedNumToString(4321));
+
+console.log(signedNumToString(-4321));
